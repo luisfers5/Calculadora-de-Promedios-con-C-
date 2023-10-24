11640a1767d19407cfe8b09e0761f3eacff8499d
Luis Fernando Sanchez <lf.sanchezgutierrez@ugto.mx>
2023-10-23 19:33:38 -0600

    First commit

---
+++ Calculadora de Promedios con C#/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace Calculadora_de_Promedios_con_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingreso = "0";
+            int contador = 0;
+            int total = 0;
+            int numeroActual = 0;
+
+            while (ingreso != "fin")
+            {
+                Console.WriteLine("El número actual es {0}", numeroActual);
+                Console.WriteLine("La cantidad de calificaciones ingresadas es de {0}", contador);
+                Console.WriteLine("Cuando desee finalizar la carga de datos, escriba 'fin'");
+                Console.WriteLine("Por favor ingrese la siguiente calificación");
+
+                ingreso = Console.ReadLine();
+
+                if (ingreso.Equals("fin"))
+                {
+                    Console.WriteLine("----------------------------------------");
+                    double promedio = (double)total / (double)contador;
+                    Console.WriteLine("El promedio obtenido por el alumno fue {0}", promedio);
+                }
+
+                if (int.TryParse(ingreso, out numeroActual) && numeroActual > 0 && numeroActual < 11)
+                {
+                    total += numeroActual;
+                }
+                else
+                {
+                    if (!(ingreso.Equals("fin")))
+                    {
+                        Console.WriteLine("Por favor ingrese un número entre 1 y 10");
+
+                    }
+                    continue;
+                }
+                contador++;
+
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Calculadora-de-Promedios-con-C-
